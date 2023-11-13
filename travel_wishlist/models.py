b8bcf521031e7d9bbdef4a00224d82a4c1be2450
ju957 <ju9572fp@go.minnstate.edu>
2023-11-12 21:23:29 -0600

    Add user accounts

--- travel_wishlist/models.py
+++ travel_wishlist/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-"""Place object used to link a place's name and whether it's been visited. """
+"""Place object used to link a place's name and whether it's been visited. Also has a user's key to allow for user-specific
+ wish lists, when they may have visited the place, notes on said place, and a photo if one is available."""
 class Place(models.Model):
+    user = models.ForeignKey('auth.User', null=False, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     visited = models.BooleanField(default=False)
+    notes = models.TextField(blank=True, null=True)
+    date_visited = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='user_images/', blank=True,  null=True)
 
-    # Gives basic info on a place and whether the user has visited it. Not intended to be seen by users.
+    # Gives basic info on a place, such as who added it, its name, and details if possible. Not intended to be seen by users.
     def __str__(self):
-        return f'{self.name} visited? {self.visited}'
+        photo_str = self.photo.url if self.photo else 'No photo found.'
+        notes_str = self.notes[100:] if self.notes else 'No notes.'
+        return f'{self.pk}: {self.name} visited? {self.visited} on {self.date_visited}\nPhoto {photo_str}'
--- travel_wishlist/views.py
+++ travel_wishlist/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
+from django.contrib.auth.decorators import login_required
+
+# @login_required will only allow the function to be called when someone is logged in.
 
 # Default template to be shown an average user
+@login_required()
 def place_list(request):
 
     if request.method == 'POST':
         # Creates a place and adds it to the database if it is a valid entry.
         form = NewPlaceForm(request.POST)
-        place = form.save()
+        place = form.save(commit=False)
+        place.user = request.user
         if form.is_valid():
             place.save()
             return redirect('place_list')  # updates the home page
 
-    places = Place.objects.filter(visited=False).order_by('name')
+    places = Place.objects.filter(user=request.user).filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm()  # Used to create HTML form.
     return render(request, 'wishlist.html', {'places': places, 'new_place_form': new_place_form})
 
 
 """Gets all of the places in the database that HAVE BEEN visited to be displayed, using visited.html as a template for
 formatting."""
+@login_required()
 def places_visited(request):
     visited = Place.objects.filter(visited=True)
     return render(request, 'visited.html', {'visited': visited})
@@ -27,6 +33,7 @@ def places_visited(request):
 
 """This function changes the status of a place from unvisited TO visited, and makes a change to the database entry.
 Error handling for nonexistent locations is present."""
+@login_required()
 def place_was_visited(request, place_pk):
     if request.method == 'POST':
         place = get_object_or_404(Place, pk=place_pk)
